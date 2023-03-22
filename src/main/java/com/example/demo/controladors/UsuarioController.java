95fae6b8b821675346b6b68028e2eb3e71d7c769
sergiogavi <sergio.gavi9@gmail.com>
2023-03-22 17:38:01 +0100

    Añadiendo logica y consulta en el back end con JS

--- src/main/java/com/example/demo/controladors/UsuarioController.java
+++ src/main/java/com/example/demo/controladors/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 class UsuarioController {
 
@@ -20,6 +23,39 @@ class UsuarioController {
         usuario.setTelefono("999342567");
         return usuario;
     }
+
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(){
+        List <Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Sergio");
+        usuario.setApellido("Garcia");
+        usuario.setEmail("sergio.gavi9@hotmail.cat");
+        usuario.setTelefono("999342567");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("Antonio");
+        usuario2.setApellido("Garcia");
+        usuario2.setEmail("sergio.gavi9@hotmail.cat");
+        usuario2.setTelefono("999342567");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("José Juan");
+        usuario3.setApellido("Garcia");
+        usuario3.setEmail("sergio.gavi9@hotmail.cat");
+        usuario3.setTelefono("999342567");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
+    }
+
 
     @RequestMapping(value = "usuario/202")
     public Usuario CreateUsuario(){
