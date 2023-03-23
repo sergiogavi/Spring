4cbb610a9f8fb45314ae95bdb3d68e24a5ce365f
sergiogavi <sergio.gavi9@gmail.com>
2023-03-23 09:52:12 +0100

    Añadiendo dependencias y uso de lombok para simplificar codigo

--- src/main/java/com/example/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/example/demo/dao/UsuarioDaoImp.java
@@ -1,16 +1,23 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-
+@Transactional
 public class UsuarioDaoImp implements UsuarioDao{
-
+    @PersistenceContext
+    //Entity manager sirve para realizar la conexión con la BD
+    private EntityManager entityManager;
     @Override
     public List<Usuario> getUsuario() {
-        return null;
+        String query= "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+
     }
 }
